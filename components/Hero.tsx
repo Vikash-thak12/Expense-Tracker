b97cd65615f92b98280b9505f60063cb5d6a7948
Vikash Thakur <thakvikash622@gmail.com>
2024-09-01 22:39:29 +0530

    Minor changeS

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Hero = () => {
@@ -8,7 +9,7 @@ const Hero = () => {
                     <h1
                         className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
                     >
-                       Expense Tracker
+                        Expense Tracker
 
                         <span className="sm:block pb-5"> Budget Better, Live Better </span>
                     </h1>
@@ -16,22 +17,15 @@ const Hero = () => {
                     <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
                         Track Every Penny, Save Every Money  – Master Your Budget and Control Your Future.
                     </p>
-
                     <div className="mt-8 flex flex-wrap justify-center gap-4">
-                        <a
-                            className="block w-full rounded border border-black bg-black px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-                            href="#"
+                        <Link
+                            className="block w-full rounded-full bg-black px-12 py-3 text-sm font-medium text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-0 active:text-opacity-75 sm:w-auto"
+                            href="/dashboard"
                         >
                             Get Started
-                        </a>
-
-                        <a
-                            className="block w-full rounded border border-black px-12 py-3 text-sm font-medium text-white hover:bg-black focus:outline-none focus:ring active:bg-gray-700 sm:w-auto"
-                            href="#"
-                        >
-                            Learn More
-                        </a>
+                        </Link>
                     </div>
+
                 </div>
             </div>
         </section>
