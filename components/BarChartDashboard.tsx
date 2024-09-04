fc708f9b14507601e66efc64c29b9d624cfbdbbf
Vikash Thakur <thakvikash622@gmail.com>
2024-09-04 14:51:43 +0530

    Updated: UI

--- components/BarChartDashboard.tsx
+++ components/BarChartDashboard.tsx
@@ -32,7 +32,7 @@ const BarChartDashboard = ({ budgetList }: { budgetList: BudgetItemProps[] }) =>
         <div className='border border-black rounded-lg p-2 bg-white'>
             <h1 className='font-bold text-center mb-3 text-2xl'>Budget Visualization</h1>
             <div className="w-full">
-                <ResponsiveContainer width="100%" height={400}>
+                <ResponsiveContainer width="100%" height={380}>
                     <BarChart
                         data={budgetList}
                         margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
@@ -42,14 +42,16 @@ const BarChartDashboard = ({ budgetList }: { budgetList: BudgetItemProps[] }) =>
                                 dataKey="name"
                                 tick={{ fontSize: 12 }} // Adjust font size for better fitting
                                 interval={0} // Show all labels
-                                className='text-green'
                             />
                         )}
                         <YAxis />
-                        {/* <Tooltip /> */}
+                        <Tooltip />
+                        {/* {
+                            isSmallScreen &&  <Tooltip />
+                        } */}
                         <Legend />
-                        <Bar dataKey="totalSpend" name='Total Spent' stackId="a" fill="green" />
-                        <Bar dataKey="amount" name='Total Amount' stackId="a" fill="gray" />
+                        <Bar dataKey="totalSpend" name='Total Spent' stackId="a" fill="#1a57a1" />
+                        <Bar dataKey="amount" name='Total Amount' stackId="a" fill="#8d1b4d" />
                     </BarChart>
                 </ResponsiveContainer>
             </div>
--- components/BudgetItem.tsx
+++ components/BudgetItem.tsx
@@ -22,41 +22,41 @@ const BudgetItem = ({ budget }: { budget: BudgetItemProps }) => {
         <Link href={`/dashboard/expense/${budget?.id}`} >
             <div className='border p-3 rounded-lg cursor-pointer bg-gray-50 lg:h-[170px]'>
 
-            <div className='flex gap-4 items-center justify-between rounded-md p-2 shadow-md shadow-black'>
-                <div className='flex gap-4 items-center'>
-                    <h1 className='border text-3xl p-3 rounded-full'>{budget.icon}</h1>
-                    <div>
-                        <h2 className='line-clamp-1'>{budget.name}</h2>
-                        <h2>{budget.totalItem} {budget.totalItem > 1 ? "Items" : "Item"}</h2>
+                <div className='flex gap-4 items-center justify-between rounded-md p-2 shadow-md shadow-black'>
+                    <div className='flex gap-4 items-center'>
+                        <h1 className='border text-3xl p-3 rounded-full'>{budget.icon}</h1>
+                        <div>
+                            <h2 className='line-clamp-1'>{budget.name}</h2>
+                            <h2>{budget.totalItem} {budget.totalItem > 1 ? "Items" : "Item"}</h2>
+                        </div>
                     </div>
+                    <h2>${budget.amount}</h2>
                 </div>
-                <h2>${budget.amount}</h2>
-            </div>
-            <div className='mt-2'>
-                <div className='flex justify-between items-center py-3'>
-                    <div className='flex flex-col lg:flex-row lg:gap-1'>
-                        <h1 className='text-gray-500'>$ {budget.totalSpend ? budget.totalSpend : 0}</h1>
-                        <span>Spent</span>
+                <div className='mt-2'>
+                    <div className='flex justify-between items-center py-3'>
+                        <div className='flex flex-col lg:flex-row lg:gap-1'>
+                            <h1 className='text-gray-500'>$ {budget.totalSpend ? budget.totalSpend : 0}</h1>
+                            <span>Spent</span>
+                        </div>
+                        <div className='flex flex-col lg:flex-row lg:gap-1'>
+                            <h1 className='text-gray-500'>$ {budget.amount - budget.totalSpend} </h1>
+                            <span>Remaining</span>
+                        </div>
                     </div>
-                    <div className='flex flex-col lg:flex-row lg:gap-1'>
-                        <h1 className='text-gray-500'>$ {budget.amount - budget.totalSpend} </h1>
-                        <span>Remaining</span>
-                    </div>
-                </div>
-                <div className='w-full bg-slate-300 h-2 rounded-full'>
-                    <div
-                        className={`bg-blue-600 h-2 rounded-full`}
-                        style={{
-                            width: `${calculateProgressPercentage()}%`,
-                            backgroundColor: 'blue',
-                            transition: 'width 1s ease-in-out'
-                        }}
+                    <div className='w-full bg-slate-300 h-2 rounded-full'>
+                        <div
+                            className={`bg-blue-600 h-2 rounded-full`}
+                            style={{
+                                width: `${calculateProgressPercentage()}%`,
+                                backgroundColor: 'blue',
+                                transition: 'width 1s ease-in-out'
+                            }}
                         >
 
+                        </div>
                     </div>
                 </div>
-            </div>
-                            
+
             </div>
         </Link>
     )
