9da28813e9deaadf00498399dad99f7dacb13ab1
Vikash Thakur <thakvikash622@gmail.com>
2024-09-04 10:27:08 +0530

    Fixed: Header for Small devices

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 import React from 'react';
 import { auth } from '@clerk/nextjs/server';
 import { UserButton } from '@clerk/nextjs';
@@ -8,34 +8,64 @@ const Header = () => {
   const { userId } = auth();
 
   return (
-    <header className='lg:hidden sticky top-0 left-0 right-0 mx-auto border-2 border-black px-4 py-2 lg:py-4 lg:w-[35%] w-[90%] mt-4 mb-2 rounded-full bg-gray-400 z-50'>
-      <div className='flex items-center justify-between lg:px-10'>
-        <Link href="/">
-          <Image
+    <header className='lg:hidden sticky top-0 left-0 right-0 mx-auto border-2 border-black px-3 py-2 lg:py-4 lg:w-[35%] w-[95%] mt-4 mb-2 rounded-full bg-gray-400 z-50'>
+      <div className='flex items-center justify-between gap-2'>
+        {/* <Link href="/"> */}
+        {/* <Image
             src='./assets/logo.svg'
             alt='logo'
             width={70}
             height={25}
             className='cursor-pointer'
-          />
-        </Link>
-        <nav className='flex items-center space-x-3 lg:space-x-5'>
+          /> */}
+        {/* <span>Home</span> */}
+        {/* </Link> */}
+        {/* <nav className='flex items-center space-x-1 lg:space-x-5'>
+          <Link href="/" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
+            <span className='text-sm font-bold'>Home</span>
+          </Link>
           <Link href="/dashboard" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
-            <span className='text-sm font-bold'>Dashboard</span>
+            <span className='text-sm font-bold'>DashBoard</span>
           </Link>
           <Link href="/dashboard/budget" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
             <span className='text-sm font-bold'>Budget</span>
           </Link>
-        </nav>
+        </nav> */}
         {
           userId !== null ? (
-            <div className='flex items-center justify-center transform scale-150'>
-              <UserButton />
-            </div>
+            <>
+              <nav className='flex items-center space-x-2 ml-1'>
+                <Link href="/" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
+                  <span className='text-sm font-bold'>Home</span>
+                </Link>
+                <Link href="/dashboard" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
+                  <span className='text-sm font-bold'>DashBoard</span>
+                </Link>
+                <Link href="/dashboard/budget" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
+                  <span className='text-sm font-bold'>Budget</span>
+                </Link>
+              </nav>
+              <div className='flex items-center justify-center scale-150 mx-auto'>
+                <UserButton />
+              </div>
+            </>
           ) : (
-            <Link href="/sign-in" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
-              <span className='text-sm font-bold'>Login</span>
-            </Link>
+            <>
+              <nav className='flex items-center space-x-1 lg:space-x-5'>
+                <Link href="/" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
+                  <span className='text-sm font-bold'>Home</span>
+                </Link>
+                <Link href="/dashboard" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
+                  <span className='text-sm font-bold'>DashBoard</span>
+                </Link>
+                <Link href="/dashboard/budget" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
+                  <span className='text-sm font-bold'>Budget</span>
+                </Link>
+              </nav>
+              <Link href="/sign-in" className='py-3 px-3 lg:px-5 rounded-full bg-gray-900 text-white hover:bg-gray-700'>
+                <span className='text-sm font-bold'>Login</span>
+              </Link>
+            </>
           )
         }
       </div>
